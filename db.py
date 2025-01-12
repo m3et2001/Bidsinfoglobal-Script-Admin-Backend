3cf6f0952d6cfcd6164e892f864f8dd7e2c817d2
m3et2001 <meetvelani2728@gmail.com>
2025-01-12 15:43:34 +0530

    wqe

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ import os
 MONGO_URI = os.getenv("MONGODB_CONNECTION_URI", "mongodb://localhost:27017")
 DATABASE_NAME = os.getenv("DB_NAME", "test")
 
-client = MongoClient(MONGO_URI)
-db = client[DATABASE_NAME]
+client = MongoClient("mongodb+srv://bidsinfoglobal:3N4ZRDaS6H64GajL@qa.t5cmca1.mongodb.net")
+db = client["script"]
 
 def get_database():
     return db
